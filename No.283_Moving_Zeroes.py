6d63dfc12edb331000e9c03caba44201e15298b0
Hong-ying-Young <hongyiny@uci.edu>
2015-09-23 16:14:12 -0700

    Given an array nums, write a function to move all 0's to the end of it

---
+++ No.283_Moving_Zeroes.py
@@ -0,0 +1,12 @@
+#No.283_Moving_Zeroes
+#Given an array nums, write a function to move all 0's to the end of it 
+#while maintaining the relative order of the non-zero elements.
+#For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+#Note:You must do this in-place without making a copy of the array. Minimize the total number of operations.
+class Solution(object):
+    def moveZeroes(self, nums):
+        y = 0
+        for x in range(len(nums)):
+            if nums[x]:
+                nums[x], nums[y] = nums[y], nums[x]
+                y += 1
